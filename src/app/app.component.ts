35b25cb42c325dcfb91f142447cd54fb1159cc30
Onuralp Sener <onuralp.sener@adesso.com.tr>
2019-02-08 10:35:14 +0300

    text datepicker checkbox done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MasterDataTable} from './models/object-config-model';
+import config from '../assets/config.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'form-app';
+
+  public object = {
+    test: 'value',
+    test2: new Date(),
+    test3: false,
+    test4: 'testValue'
+  };
+
+  public config: MasterDataTable;
+
+
+  constructor() {
+    this.config = config;
+  }
+
+
 }
